eeb0a408c0f8f543ed9f8d50d7ed69d19f975c50
Roxanna KL <30495388+rskl92@users.noreply.github.com>
2019-11-26 15:20:47 +0000

    Update formatting_plink_files.R

--- Code/Meta-analysis_IGAP_PGC_ADSP/formatting_plink_files.R
+++ Code/Meta-analysis_IGAP_PGC_ADSP/formatting_plink_files.R
@@ -1,6 +1,6 @@
 library(data.table)
-setwd("${HOME}/Alzheimers_datasets/")
-metaanalysis <- fread("${HOME}/Alzheimers_datasets/plink.meta")
+setwd("/pathname/Alzheimers_datasets/")
+metaanalysis <- fread("/pathname/Alzheimers_datasets/plink.meta")
 
 metaanalysis$SE <- abs(plink$BETA/qnorm(plink$P/2))
 
@@ -10,7 +10,7 @@ metaanalysis$Q<- NULL
 metaanalysis$I <- NULL
 
 ## PROBLEMATIC SNPs identified
-metaanalysis_problematic_snps <- fread(${HOME}/Alzheimers_datasets/plink.prob", he=F)
+metaanalysis_problematic_snps <- fread("/pathname/Alzheimers_datasets/plink.prob", he=F)
 colnames(metaanalysis_problematic_snps)[2] <- "snp"
 write.table(metaanalysis_problematic_snps$snp, "metaanalysis_problematic_snps.txt", row.names=F)
 
